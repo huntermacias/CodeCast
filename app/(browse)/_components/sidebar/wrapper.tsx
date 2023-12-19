a642191f9f8d86956a60e61a5e450c3670d518e4
huntermacias <supercodersf@gmail.com>
2023-12-18 22:29:42 -0800

    feat: recommended list

--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -1,23 +1,38 @@
-"use client"
+"use client";
 
 import { useSidebar } from "@/store/use-sidebar";
 import { cn } from "@/lib/utils";
+import { useEffect, useState } from "react";
+import { ToggleSkeleton } from "./toggle";
+import { RecommendedSkeleton } from "./recommended";
+import { useIsClient } from "usehooks-ts";
 
 interface WrapperProps {
-	children: React.ReactNode
-};
+  children: React.ReactNode;
+}
 
 export const Wrapper = ({ children }: WrapperProps) => {
+  // const [isClient, setIsClient] = useState(false);
+  const isClient = useIsClient();
 
-	const { collapsed } = useSidebar((state) => state);
+  const { collapsed } = useSidebar((state) => state);
+
+  if (!isClient)
+    return (
+      <aside className="fixed left-0 flex flex-col lg:w-60 h-full bg-background border-r border-[#2d2e35] z-50">
+        <ToggleSkeleton />
+        <RecommendedSkeleton />
+      </aside>
+    );
 
   return (
-	<aside className={cn(
-		"fixed left-0 flex flex-col w-60 h-full bg-background border-r border-[#2d2e35] z-50", 
-		collapsed && "w-[70px]")
-	}>
-		
-		{children}
-	</aside>
-  )
-}
+    <aside
+      className={cn(
+        "fixed left-0 flex flex-col w-60 h-full bg-background border-r border-[#2d2e35] z-50",
+        collapsed && "w-[70px]"
+      )}
+    >
+      {children}
+    </aside>
+  );
+};
--- lib/recommended-service.ts
+++ lib/recommended-service.ts
@@ -1,14 +1,43 @@
 
 import { db } from "./db";
 import { getSelf } from "./auth-service";
+import { resolve } from "path";
+import { Lekton } from "next/font/google";
 
 
 export const getRecommended = async () => {
-	const users = await db.user.findMany({
-		orderBy: {
-			createdAt: "desc"
-		},
-	});
+
+	let userId;
+	try {
+		const self = await getSelf();
+		userId = self.id;
+	} catch (e) {
+		userId = null;
+	}
+
+	let users = [];
+	if (userId){
+		users = await db.user.findMany({
+			orderBy: {
+				createdAt: "desc"
+			},
+			where: {
+				id: {
+					not: userId
+				}
+			}
+		});
+	} else {
+
+		// await new Promise(resolve => setTimeout(resolve, 2000));
+		users = await db.user.findMany({
+			orderBy: {
+				createdAt: "desc"
+			},
+		});
+	}
+
+
 
 	return users;
 }
