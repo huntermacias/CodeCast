336863da6951d171479b1d214eb406aa40dda265
huntermacias <supercodersf@gmail.com>
2023-12-19 17:43:45 -0800

    feat: livkit webhook & viewer token

--- app/(browse)/_components/sidebar/recommended.tsx
+++ app/(browse)/_components/sidebar/recommended.tsx
@@ -1,51 +1,49 @@
 "use client";
 
+import { User } from "@prisma/client";
+
 import { useSidebar } from "@/store/use-sidebar";
-import { User } from "@prisma/client"
+
 import { UserItem, UserItemSkeleton } from "./user-item";
 
 interface RecommendedProps {
-	data: User[];
+  data: (User & {
+    stream: { isLive: boolean } | null;
+  })[];
 }
 
-export const Recommended = ({data}: RecommendedProps) => {
+export const Recommended = ({ data }: RecommendedProps) => {
+  const { collapsed } = useSidebar((state) => state);
 
-	const { collapsed } = useSidebar((state) => state);
+  const showLabel = !collapsed && data.length > 0;
 
-	const showLabel = !collapsed && data.length > 0;
-
-	// console.log(data.length);
-
-	
-
-	return (
-		<div>
-			{showLabel && (
-				<div className="pl-6 mb-4">
-					<p className="text-sm text-muted-foreground">Recommended</p>
-				</div>
-			)}
-
-			<ul className="space-y-2 px-2">
-				{data.map((user) => (
-					<UserItem 
-						key={user.id}
-						username={user.username}
-						imageUrl={user.imageUrl}
-						isLive={true}
-					/>
-				))}
-			</ul>
-		</div>
-	)
-}
+  return (
+    <div>
+      {showLabel && (
+        <div className="pl-6 mb-4">
+          <p className="text-sm text-muted-foreground">Recommended</p>
+        </div>
+      )}
+      <ul className="space-y-2 px-2">
+        {data.map((user) => (
+          <UserItem
+            key={user.id}
+            username={user.username}
+            imageUrl={user.imageUrl}
+            isLive={user.stream?.isLive}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export const RecommendedSkeleton = () => {
-	return (
-		<ul className="px-2">
-			{[...Array(3)].map((_, i) => (
-				<UserItemSkeleton key={i} />
-			))}
-		</ul>
-	)
-}
+  return (
+    <ul className="px-2">
+      {[...Array(3)].map((_, i) => (
+        <UserItemSkeleton key={i} />
+      ))}
+    </ul>
+  );
+};
--- app/(dashboard)/u/[username]/(home)/page.tsx
+++ app/(dashboard)/u/[username]/(home)/page.tsx
@@ -1,34 +1,27 @@
 import { currentUser } from "@clerk/nextjs";
 
 import { getUserByUsername } from "@/lib/user-service";
-// import { StreamPlayer } from "@/components/stream-player";
+import { StreamPlayer } from "@/components/stream-player";
 
 interface CreatorPageProps {
   params: {
     username: string;
   };
-};
+}
 
-const CreatorPage = async ({
-  params,
-}: CreatorPageProps) => {
+const CreatorPage = async ({ params }: CreatorPageProps) => {
   const externalUser = await currentUser();
   const user = await getUserByUsername(params.username);
 
-//   if (!user || user.externalUserId !== externalUser?.id || !user.stream) {
-//     throw new Error("Unauthorized");
-//   }
+  if (!user || user.externalUserId !== externalUser?.id || !user.stream) {
+    throw new Error("Unauthorized");
+  }
 
-  return ( 
+  return (
     <div className="h-full">
-		stream player
-      {/* <StreamPlayer
-        user={user}
-        stream={user.stream}
-        isFollowing
-      /> */}
+      <StreamPlayer user={user} stream={user.stream} isFollowing />
     </div>
   );
-}
- 
+};
+
 export default CreatorPage;
